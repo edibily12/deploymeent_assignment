0ec705e8a0afcfaab4046bbfcff8c7a8779b9e3d
edibily12 <edibily06@gmail.com>
2025-03-28 05:07:22 +0300

    add README.md

--- index.php
+++ index.php
@@ -5,3 +5,6 @@ header('Content-Type: application/json');
 
 $api = new UniversityAPI();
 $api->processRequest();
+
+//clone nohup php -S 0.0.0.0:8080 > /dev/null 2>&1 &
+//kill pkill -f "php -S 0.0.0.0:8080"
